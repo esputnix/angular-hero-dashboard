3a0d70d67b632174879e410c55c7b9e4632cac6c
esputnix <pasha.ivanoff@icloud.com>
2020-06-16 21:14:52 -0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Tour of Heroes';
+
+  onClick() {
+    console.log("...onClick");
+  }
 }
-
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
