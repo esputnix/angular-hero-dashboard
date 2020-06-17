5389cb5e77e5bf5751ecba4fb1727ff563a884b3
esputnix <pasha.ivanoff@icloud.com>
2020-06-16 21:28:32 -0700

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
-
+import { AppRoutingModule }     from './app-routing.module';
 import { AppComponent }         from './app.component';
 import { DashboardComponent }   from './dashboard/dashboard.component';
 
@@ -9,6 +9,7 @@ import { DashboardComponent }   from './dashboard/dashboard.component';
   imports: [
     BrowserModule,
     FormsModule,
+    AppRoutingModule,
   ],
   declarations: [
     AppComponent,
